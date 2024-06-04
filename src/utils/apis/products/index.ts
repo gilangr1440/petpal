18762d6ddba526a56b7e8ae2ce56c688e92def89
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-06-05 02:02:54 +0700

    64-issues-detail-product

--- src/utils/apis/products/index.ts
+++ src/utils/apis/products/index.ts
@@ -1,6 +1,18 @@
-import { getProductDetail, getProducts, addProduct, getProductsAdmin } from "./api";
+import { getProducts, addProduct, getProductsAdmin } from "./api";
 import { productSchema } from "./scheme";
-import { ProductFormValues, ProductFormProps, IProductListData, IProductDetail, ProductAdmin } from "./interfaces";
+import {
+  ProductFormValues,
+  ProductFormProps,
+  IProductListData,
+  IProductDetail,
+  ProductAdmin,
+} from "./interfaces";
 
-export { productSchema, getProducts, getProductDetail, addProduct, getProductsAdmin };
-export type { ProductFormValues, ProductFormProps, IProductListData, IProductDetail, ProductAdmin };
+export { productSchema, getProducts, addProduct, getProductsAdmin };
+export type {
+  ProductFormValues,
+  ProductFormProps,
+  IProductListData,
+  IProductDetail,
+  ProductAdmin,
+};
