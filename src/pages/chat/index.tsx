fa8c8d82fa42ebea363becc70f06d901c0ab270a
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-06-05 03:04:20 +0700

    issues: udah bisa Create Read nya tinggal Update Delete nya, dan belum bisa milih berdasarkan roomchat

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -6,11 +6,12 @@ import { Menu, Send } from "lucide-react";
 import { useEffect, useState } from "react";
 
 import axiosWithConfig from "@/utils/apis/axiosWithConfig";
+import { Button } from "@/components/ui/button";
 
 const getChatMessages = async (chatId: number) => {
   try {
     const response = await axiosWithConfig.get(
-      `http://zyannstore.my.id/chats/${chatId}`
+      `${import.meta.env.VITE_BASE_URL}/chats/${chatId}`
     );
     return response.data;
   } catch (error) {
@@ -19,27 +20,49 @@ const getChatMessages = async (chatId: number) => {
   }
 };
 
+const postChatMessage = async (chatId: number, message: string) => {
+  try {
+    const response = await axiosWithConfig.post(
+      `${import.meta.env.VITE_BASE_URL}/chats/${chatId}`,
+      { message }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error sending chat message:", error);
+    return null;
+  }
+};
+
 const Chat = () => {
   const [sideMenu, setSideMenu] = useState<boolean>(false);
   const [messages, setMessages] = useState<any[] | null>([]);
-  const [cookies, setCookie, removeCookie] = useCookies();
-
+  const [newChat, setNewChat] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [cookies] = useCookies();
   const loginId = cookies.login_id;
 
-  if (loginId) {
-    console.log(`Login ID: ${loginId}`);
-  } else {
-    console.log("No login ID found in cookies");
-  }
+  const getMessages = async () => {
+    setLoading(true);
+    const chatId = 1;
+    const messages = await getChatMessages(chatId);
+    setMessages(messages.data);
+    setLoading(false);
+  };
+
+  const sendMessage = async () => {
+    setLoading(true);
+    const chatId = 1;
+    const response = await postChatMessage(chatId, newChat);
+    if (response) {
+      setNewChat("");
+      getMessages();
+    } else {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const chatId = 1;
-      const messages = await getChatMessages(chatId);
-      setMessages(messages.data);
-    };
-
-    fetchData();
+    getMessages();
   }, []);
 
   useEffect(() => {
@@ -116,7 +139,7 @@ const Chat = () => {
                   }`}
                 >
                   <div
-                    className={`w-[45%] bg-${
+                    className={`w-fit max-w-[45%] bg-${
                       loginId === message.sender_id ? "[#9ED1E3]" : "[#C6D6CE]"
                     } p-3 rounded-md`}
                   >
@@ -129,8 +152,36 @@ const Chat = () => {
             )}
           </div>
           <div className="w-full flex items-center gap-3 p-3">
-            <Input placeholder="type some text" />
-            <Send />
+            <Input
+              value={newChat}
+              onChange={(e) => setNewChat(e.target.value)}
+              placeholder="type some text"
+            />
+            <Button onClick={sendMessage} disabled={loading}>
+              {loading ? (
+                <div role="status">
+                  <svg
+                    aria-hidden="true"
+                    className="w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                  <span className="sr-only">Loading...</span>
+                </div>
+              ) : (
+                <Send />
+              )}
+            </Button>
           </div>
         </div>
       </div>
