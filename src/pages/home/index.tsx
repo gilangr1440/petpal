c6250be4c521d9a6d113bc18f26f492dd4fd6c85
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-06-05 10:46:03 +0700

    68-feature---add-loading-animation

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,16 +10,20 @@ import { sortProductsAtom } from "@/utils/jotai/atom";
 import { IProductListData, getProducts } from "@/utils/apis/products";
 import ProductList from "@/components/product-list";
 import SortProducts from "@/components/sort-product";
+import Loaders from "@/components/loaders";
 
 const Home = () => {
   const [data, setData] = useState<IProductListData[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
   const [sort] = useAtom(sortProductsAtom);
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
         const products = await getProducts(`${sort && `sort=${sort}`}`);
         setData(products.data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching products:", error);
       }
@@ -34,12 +38,24 @@ const Home = () => {
           <Hero />
         </div>
       </section>
-      <section className="container mx-auto h-full flex items-start justify-center flex-col gap-y-8 py-6">
-        <header className="w-full flex flex-col items-start sm:items-end justify-between gap-y-4 sm:flex-row">
-          <CategoryTabs />
-          <SortProducts />
-        </header>
-        <ProductList data={data} />
+      <section className="relative container mx-auto h-full flex items-start justify-center flex-col gap-y-8 py-6">
+        {loading ? (
+          <Loaders className="absolute top-full left-1/2 -translate-y-1/2 -translate-x-1/2" />
+        ) : (
+          <>
+            <header
+              className={`w-full flex flex-col ${
+                loading
+                  ? "items-center justify-center"
+                  : "items-start justify-between"
+              } sm:items-end  gap-y-4 sm:flex-row`}
+            >
+              <CategoryTabs />
+              <SortProducts />
+            </header>
+            <ProductList data={data} />
+          </>
+        )}
       </section>
     </Layout>
   );
