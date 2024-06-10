aa444341ff0648d396ee2d3e0d3214378a454a29
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-06-10 07:00:26 +0700

    added loading and handle if data null

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -1,4 +1,5 @@
 import Layout from "@/components/layout";
+import Loaders from "@/components/loaders";
 import {
   Table,
   TableBody,
@@ -13,41 +14,61 @@ import { useEffect, useState } from "react";
 
 const History = () => {
   const [orderItem, setOrderItem] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
   useEffect(() => {
     orderHistory().then((response) => {
       setOrderItem(response.data);
+      setLoading(false);
     });
   }, []);
 
+  if (loading) {
+    return (
+      <Loaders className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2" />
+    );
+  }
   return (
     <Layout>
       <div className="w-4/5 mx-auto my-10">
         <h1 className="text-2xl font-semibold">Payment History</h1>
         <div className="my-5">
-          <Table>
-            <TableCaption>A list of your recent invoices.</TableCaption>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Product Name</TableHead>
-                <TableHead>Quantity</TableHead>
-                <TableHead>Payment Method</TableHead>
-                <TableHead>Virtual Code</TableHead>
-                <TableHead>Amount</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {orderItem &&
-                orderItem.map((item) => (
-                  <TableRow key={item.id}>
-                    <TableCell>{item.product_name}</TableCell>
-                    <TableCell>{item.quantity}</TableCell>
-                    <TableCell>{item.payment.payment_status}</TableCell>
-                    <TableCell>{item.payment.va_number}</TableCell>
-                    <TableCell>{item.price}</TableCell>
-                  </TableRow>
-                ))}
-            </TableBody>
-          </Table>
+          {orderItem && orderItem != null ? (
+            <Table>
+              <TableCaption>A list of your recent invoices.</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Product Name</TableHead>
+                  <TableHead>Quantity</TableHead>
+                  <TableHead>Payment Method</TableHead>
+                  <TableHead>Virtual Code</TableHead>
+                  <TableHead>Amount</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {orderItem.map((item) => {
+                  return (
+                    <TableRow key={item.id}>
+                      <TableCell>{item.product_name}</TableCell>
+                      <TableCell>{item.quantity}</TableCell>
+                      <TableCell>{item.payment.payment_status}</TableCell>
+                      <TableCell>{item.payment.va_number}</TableCell>
+                      <TableCell>{item.price}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          ) : (
+            <div className="w-full h-full flex items-center justify-center flex-col">
+              <img
+                src="/public/assets/data-not-dound.png"
+                alt=""
+                className="mx-auto"
+              />
+              <h1>You dont have Order History</h1>
+            </div>
+          )}
         </div>
       </div>
     </Layout>
