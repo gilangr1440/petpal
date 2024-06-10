c3fd544b5abb83b6a1b1b7a7db9786012d28f845
gilangr1440 <gilangr1440@gmail.com>
2024-06-10 18:25:29 +0700

    fix image

--- src/pages/home/category-tabs.tsx
+++ src/pages/home/category-tabs.tsx
@@ -7,28 +7,18 @@ interface CategoryTabsProps {
 
 const CategoryTabs = ({ onTabChange }: CategoryTabsProps) => {
   return (
-    <Tabs
-      defaultValue="petshop"
-      className="w-[400px] h-full flex items-start justify-start flex-col gap-y-2"
-      onValueChange={onTabChange}
-    >
+    <Tabs defaultValue="petshop" className="w-[400px] h-full flex items-start justify-start flex-col gap-y-2" onValueChange={onTabChange}>
       <h1 className="text-base font-semibold">Category</h1>
       <TabsList>
-        <TabsTrigger
-          value="petshop"
-          className="flex items-center justify-center flex-col gap-y-[2px]"
-        >
+        <TabsTrigger value="petshop" className="flex items-center justify-center flex-col gap-y-[2px]">
           <Avatar>
-            <AvatarImage src="/public/assets/petshop-icon.png" />
+            <AvatarImage src="/assets/petshop-icon.png" />
           </Avatar>
           <h1>Pet Shop</h1>
         </TabsTrigger>
-        <TabsTrigger
-          value="clinic"
-          className="flex items-center justify-center flex-col gap-y-[2px]"
-        >
+        <TabsTrigger value="clinic" className="flex items-center justify-center flex-col gap-y-[2px]">
           <Avatar>
-            <AvatarImage src="/public/assets/check-up-icon.png" />
+            <AvatarImage src="/assets/check-up-icon.png" />
           </Avatar>
           <h1>Clinic</h1>
         </TabsTrigger>
