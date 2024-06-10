9bf466c8dcb584daedea82e0a8fb4f7b2df70af9
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-06-10 18:13:40 +0700

    120-add-loading-to-all-page

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -6,14 +6,17 @@ import { useEffect, useState } from "react";
 import { getConsultations } from "@/utils/apis/clinics/api";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useCookies } from "react-cookie";
-import { toast } from "@/components/ui/use-toast";
 import { Toaster } from "@/components/ui/toaster";
 import EmptyChat from "./empty-chat";
 import { IConsultation } from "@/utils/apis/chat/interfaces";
+import Loaders from "@/components/loaders";
 
 const Chat = () => {
+  const [loading, setLoading] = useState<boolean>(true);
   const [consultation, setConsultation] = useState<IConsultation[]>([]);
-  const [selectedRoomChatId, setSelectedRoomChatId] = useState<number | null>(null);
+  const [selectedRoomChatId, setSelectedRoomChatId] = useState<number | null>(
+    null
+  );
   const [sideMenu] = useAtom<boolean>(sideMenuChat);
   const [cookies] = useCookies(["role"]);
   const role = cookies.role;
@@ -23,73 +26,76 @@ const Chat = () => {
   };
 
   useEffect(() => {
-    getConsultations().then((response) => {
-      setConsultation(response.data);
-    });
+    const fetchConsultations = async () => {
+      try {
+        const response = await getConsultations();
+        setConsultation(response.data);
+      } catch (error) {
+        console.error("Error fetching consultations:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchConsultations();
   }, []);
 
-  useEffect(() => {
-    if (consultation) {
-      const pendingTransactions = consultation.filter(
-        (item) => item.transaction_status.toLowerCase() === "pending"
-      );
-      if (pendingTransactions.length > 0) {
-        toast({
-          variant: "destructive",
-          title: "You have transaction should be paid",
-        });
-      }
-    }
-  }, [consultation]);
-
-  const paidTransaction =
+  const onlineConsultation =
     consultation &&
-    consultation.filter(
-      (item) => item.transaction_status.toLowerCase() === "paid"
-    );
+    consultation.filter((item) => {
+      return item.service.toLowerCase() === "online consultation";
+    });
 
-  const haveAConvertation = paidTransaction && paidTransaction.length > 0;
+  const haveAConvertation = onlineConsultation && onlineConsultation.length > 0;
+
+  if (loading || !onlineConsultation) {
+    return (
+      <main className="relative min-h-[400px] w-full flex items-center justify-center">
+        <Loaders className="" />
+      </main>
+    );
+  }
+
+  if (!haveAConvertation || consultation === undefined) {
+    return <EmptyChat role={role} />;
+  }
 
   return (
     <>
       <Navbar />
       <Toaster />
-      {haveAConvertation ? (
-        <div className="h-[calc(100vh-68px)] flex items-start justify-start">
-          <div
-            className={`max-w-[300px] w-full h-full ${
-              sideMenu ? "flex" : "hidden"
-            } bg-[#64A1B7] md:flex flex-col gap-3 items-center p-3`}
-          >
-            {paidTransaction.map((item: IConsultation) => (
-              <div
-                key={item.id}
-                className="w-full p-3 rounded-md flex items-center gap-3 bg-[#226583] hover:bg-[#226583]/70 cursor-pointer"
-                onClick={() => handleRoomChatClick(item.id)}
-              >
-                <Avatar className="w-10 h-10">
-                  <AvatarImage
-                    src={
-                      item[role === "admin" ? "user_details" : "doctor_details"]
-                        .profile_picture
-                    }
-                    className="w-full h-full object-cover rounded-full"
-                  />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <h1 className="text-white">
-                  {role === "admin"
-                    ? item.user_details.full_name
-                    : item.doctor_details.full_name}
-                </h1>
-              </div>
-            ))}
-          </div>
-          {selectedRoomChatId && <RoomChat roomChatId={selectedRoomChatId} />}
+      <div className="h-[calc(100vh-68px)] flex items-start justify-start">
+        <div
+          className={`max-w-[300px] w-full h-full ${
+            sideMenu ? "flex" : "hidden"
+          } bg-[#64A1B7] md:flex flex-col gap-3 items-center p-3`}
+        >
+          {onlineConsultation.map((item: IConsultation) => (
+            <div
+              key={item.id}
+              className="w-full p-3 rounded-md flex items-center gap-3 bg-[#226583] hover:bg-[#226583]/70 cursor-pointer"
+              onClick={() => handleRoomChatClick(item.id)}
+            >
+              <Avatar className="w-10 h-10">
+                <AvatarImage
+                  src={
+                    item[role === "admin" ? "user_details" : "doctor_details"]
+                      .profile_picture
+                  }
+                  className="w-full h-full object-cover rounded-full"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <h1 className="text-white">
+                {role === "admin"
+                  ? item.user_details.full_name
+                  : item.doctor_details.full_name}
+              </h1>
+            </div>
+          ))}
         </div>
-      ) : (
-        <EmptyChat role={role} />
-      )}
+        {selectedRoomChatId && <RoomChat roomChatId={selectedRoomChatId} />}
+      </div>
     </>
   );
 };
--- src/utils/jotai/atom.ts
+++ src/utils/jotai/atom.ts
@@ -1,7 +1,8 @@
 import { atom } from "jotai";
 
 export const sortProductsAtom = atom<string>("");
-export const sideMenuChat = atom<boolean>(false);
+export const sideMenuChat = atom<boolean>(true);
 export const loadingChat = atom<boolean>(false);
 export const limitProducts = atom<number>(10);
 export const searchProducts = atom<string>("");
+export const loadingRoomChat = atom<boolean>(true);
